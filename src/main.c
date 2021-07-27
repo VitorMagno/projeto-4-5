43491610ed2ee07d9eeebb433bdc79a79afcf92d
Jhn <jhonata.tenorio2526@gmail.com>
2021-07-27 20:11:20 -0300

    ...

--- src/main.c
+++ src/main.c
@@ -1,1 +1,19 @@
 // main function
+
+/*
+    Implemente um banco de dados da cidade usando listas não ordenadas. 
+    Cada registro do banco de dados contém o nome da cidade (uma string de comprimento arbitrário)
+    e as coordenadas da cidade expressa como coordenadas xey inteiras. Seu banco de dados deve
+    permite que os registros sejam inseridos, excluídos por nome ou coordenada e pesquisados
+    por nome ou coordenada. Outra operação que deve ser suportada é imprimir todos os registros 
+    dentro de uma determinada distância de um ponto especificado. Implementar o banco de dados usando
+    uma implementação de lista baseada em array e, em seguida, uma implementação de lista vinculada. 
+    Colete estatísticas de tempo de execução para cada operação em ambas as implementações.
+*/
+#include <stdio.h>
+
+int main(){
+
+
+    return 0;
+}
